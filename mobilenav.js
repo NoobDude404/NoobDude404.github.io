9469181ebecb4dbc99090e1cb743f517246fefb8
NoobDude404 <119894465+NoobDude404@users.noreply.github.com>
2022-12-19 13:22:20 +0100

    Add files via upload

--- mobilenav.js
+++ mobilenav.js
@@ -1,8 +1,12 @@
 function navonclick() {
     var x = document.getElementById("me");
-    if (x.style.display === "none") {
+    if (x.style.display === "none") 
+    {
       x.style.display = "block";
-    } else {
+    } 
+    else 
+    {
       x.style.display = "none";
     }
+    console.log(x.style);
   }
